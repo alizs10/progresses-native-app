0ae91e9f724cc798e394f489cd0f30c856176ea1
alizs10 <ali.text77@gmail.com>
2024-11-06 12:49:26 +0330

    update: about screen

--- screens/AboutScreen.jsx
+++ screens/AboutScreen.jsx
@@ -1,9 +1,48 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function AboutScreen() {
     return (
-        <View>
-            <Text>AboutScreen</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>
+                By constantly remembering Long/short-term life goals and creating a feeling of pleasure by reaching them, you can actually improve your lifestyle.
+            </Text>
+
+            <Text style={styles.text}>
+                This project was developed using React Native and some other libraries to bring a wonderful UI and UX. it's easy to use thanks to the support of different gestures.
+            </Text>
+
+            <View>
+
+                <Text style={styles.text}>
+                    Developer: @alizs10
+                </Text>
+                <Text style={styles.text}>
+                    App Version: 0.0.1
+                </Text>
+                <Text style={styles.text}>
+                    Copyright © 2024 All rights reserved
+                </Text>
+            </View>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        height: '100%',
+        backgroundColor: '#0F1012',
+        paddingHorizontal: 20,
+        paddingVertical: 12,
+        flexDirection: 'column',
+        gap: 20,
+    },
+    text: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'semibold',
+        textAlign: 'justify',
+        lineHeight: 28,
+    }
+})
